d3a03e4c633f5a1f53626b939ba8f01c542a769e
santiago daniel <santi-dy240@hotmail.com>
2024-07-16 16:50:53 -0600

    add dotenv.config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import dotenv from 'dotenv'
 import connectDb from './utils/conectionDb.js'
-dotenv.config()
+dotenv.config({path:"./config.env"})
 
 connectDb(process.env.URL_DB)
 
